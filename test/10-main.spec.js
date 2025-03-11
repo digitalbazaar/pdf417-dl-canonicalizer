7bebe96ff8c7340c4efb8b6d8a7e25456baefbc8
Tyler Minard <tminard@digitalbazaar.com>
2025-03-11 15:32:34 -0400

    Add test and pull out decode.

--- test/10-main.spec.js
+++ test/10-main.spec.js
@@ -3,6 +3,7 @@
  */
 import {
   canonicalize,
+  decode,
   hash,
   parse
 } from '../lib/index.js';
@@ -11,15 +12,16 @@ import {crypto} from '../lib/platform.js';
 const encoder = new TextEncoder();
 const pdf417StringPre = '@\n';
 const recSep = String.fromCharCode(30);
+const sigData = 'NRecFIyZjocrir2Zuzp3K' +
+'DOYh30ohkVkVqJzq+Ns2kjfm2KLbDesFNsrPqIpsRueu8rb6V0tcYjyDs5jETx95EgJeCjG' +
+'mRZLzJ0WNeYcQwEDO/UuMXUwR1lFMxNic2Hx9+Z5NLNMxjt4wsk6pBEXO1Ul0T/4LFEKzRM' +
+'31Vf+z0l5x077DZwtb3V88Nwq26xaDDZZW1xAi0I99limW7fZqZ0DQFvcEeiCqTq0EvNieX' +
+'XU/sYB';
 const pdf417StringPost = '\rANSI 000000090002DL00410267ZZ03080162DLDAQ0' +
 '0000000\nDCSDOE\nDDEN\nDACJOHN\nDDFN\nDADNONE\nDDGN\nDCANONE\nDCBNONE\nDCDN' +
 'ONE\nDBD01012024\nDBB01011950\nDBA01012029\nDBC1\nDAU072 IN\nDAYBLK\nDAG' +
 '123 EXAMPLE ST\nDAIGOTHAM\nDAJNY\nDAK123450000  \nDCFTESTDOCDISCRIM\nDCG' +
-'USA\nDAW160\nDCK1234567890\nDDAN\nDDB01012000\rZZZZANRecFIyZjocrir2Zuzp3K' +
-'DOYh30ohkVkVqJzq+Ns2kjfm2KLbDesFNsrPqIpsRueu8rb6V0tcYjyDs5jETx95EgJeCjG' +
-'mRZLzJ0WNeYcQwEDO/UuMXUwR1lFMxNic2Hx9+Z5NLNMxjt4wsk6pBEXO1Ul0T/4LFEKzRM' +
-'31Vf+z0l5x077DZwtb3V88Nwq26xaDDZZW1xAi0I99limW7fZqZ0DQFvcEeiCqTq0EvNieX' +
-'XU/sYB\r';
+'USA\nDAW160\nDCK1234567890\nDDAN\nDDB01012000\rZZZZA' + sigData + '\r';
 
 const pdf417String = pdf417StringPre.concat(recSep, pdf417StringPost);
 const pdfBytes = encoder.encode(pdf417String);
@@ -51,13 +53,13 @@ describe('Canonicalizer Test', function() {
     testHash.should.be.an('uint8array');
     testCanonicalized.should.be.an('uint8array');
   });
-  it('Should canonize correctly', async function() {
+  it('Should canonicalize correctly', async function() {
     const parsedData = await parse({pdfBytes, fields});
     const canonizedBytes = canonicalize({parsedData});
 
     testBytes.should.deep.equal(canonizedBytes);
   });
-  it('Should canonize + hash correctly', async function() {
+  it('Should canonicalize + hash correctly', async function() {
     // test against different hasher here?
     const testHash = await crypto.subtle.digest('SHA-256', testBytes);
     const testArray = new Uint8Array(testHash);
@@ -75,4 +77,60 @@ describe('Canonicalizer Test', function() {
 
       testArray.should.deep.equal(canonizedHash);
     });
+  it('Should decode raw bytes', async function() {
+    const result = await decode({pdfBytes});
+
+    // Expect the optional fields too
+    const expectedDlData = {
+      DAW: '160',
+      DCK: '1234567890',
+      DDA: 'N',
+      DDB: '01012000'
+    };
+
+    const fields = orderedFieldsComplete.split('\n');
+    for(const field of fields) {
+      const key = field.slice(0, 3);
+      const value = field.slice(3);
+      if(key) {
+        expectedDlData[key] = value;
+      }
+    }
+
+    result.should.deep.equal({
+      compliance: '@',
+      aamvaVersionNumber: '09',
+      elementSeparator: '\n',
+      fileType: 'ANSI ',
+      issuerIdentificationNumber: '000000',
+      jurisdictionVersionNumber: '00',
+      numberOfEntries: 2,
+      recordSeparator: '\u001e',
+      segmentTerminator: '\r',
+      entries: [
+        {
+          type: 'DL',
+          offset: 41,
+          length: 267
+        },
+        {
+          type: 'ZZ',
+          offset: 308,
+          length: 162
+        }
+      ],
+      subfiles: [
+        {
+          type: 'DL',
+          data: expectedDlData
+        },
+        {
+          type: 'ZZ',
+          data: {
+            ZZA: sigData
+          }
+        }
+      ]
+    });
+  });
 });
