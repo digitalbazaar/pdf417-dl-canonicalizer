9791305f74320621c5da694d7387129d9d9650c0
Dave Longley <dlongley@digitalbazaar.com>
2025-04-07 16:06:00 -0400

    Change `fields` to `selector` API.

--- test/10-main.spec.js
+++ test/10-main.spec.js
@@ -27,25 +27,34 @@ const pdf417String = pdf417StringPre.concat(recSep, pdf417StringPost);
 const data = encoder.encode(pdf417String);
 
 const orderedFields =
-      'DAG123 EXAMPLE ST\n' + 'DAIGOTHAM\n' + 'DAJNY\n' +
-      'DAK123450000  \n' + 'DAQ00000000\n' + 'DAU072 IN\n' +
-      'DAYBLK\n' + 'DBA01012029\n' + 'DBB01011950\n' +
-      'DBC1\n' + 'DCGUSA\n';
-const fields = 'uP_BA';
-const orderedFieldsComplete =
-      'DACJOHN\n' + 'DADNONE\n' + 'DAG123 EXAMPLE ST\n' +
-      'DAIGOTHAM\n' + 'DAJNY\n' + 'DAK123450000  \n' + 'DAQ00000000\n' +
-      'DAU072 IN\n' + 'DAYBLK\n' + 'DBA01012029\n' +
-      'DBB01011950\n' + 'DBC1\n' + 'DBD01012024\n' + 'DCANONE\n' +
-      'DCBNONE\n' + 'DCDNONE\n' + 'DCFTESTDOCDISCRIM\n' +
-      'DCGUSA\n' + 'DCSDOE\n' + 'DDEN\n' + 'DDFN\n' + 'DDGN\n';
+  'DAG123 EXAMPLE ST\n' + 'DAIGOTHAM\n' + 'DAJNY\n' +
+  'DAK123450000  \n' + 'DAQ00000000\n' + 'DAU072 IN\n' +
+  'DAYBLK\n' + 'DBA01012029\n' + 'DBB01011950\n' +
+  'DBC1\n' + 'DCGUSA\n';
+const selector = {componentIndex: 'uP_BA'};
+const orderedFieldsMandatory =
+  'DACJOHN\n' + 'DADNONE\n' + 'DAG123 EXAMPLE ST\n' +
+  'DAIGOTHAM\n' + 'DAJNY\n' + 'DAK123450000  \n' + 'DAQ00000000\n' +
+  'DAU072 IN\n' + 'DAYBLK\n' + 'DBA01012029\n' +
+  'DBB01011950\n' + 'DBC1\n' + 'DBD01012024\n' + 'DCANONE\n' +
+  'DCBNONE\n' + 'DCDNONE\n' + 'DCFTESTDOCDISCRIM\n' +
+  'DCGUSA\n' + 'DCSDOE\n' + 'DDEN\n' + 'DDFN\n' + 'DDGN\n';
 const testBytes = encoder.encode(orderedFields);
+const testBytesMandatory = encoder.encode(orderedFieldsMandatory);
+const orderedFieldsComplete =
+  'DACJOHN\n' + 'DADNONE\n' + 'DAG123 EXAMPLE ST\n' +
+  'DAIGOTHAM\n' + 'DAJNY\n' + 'DAK123450000  \n' + 'DAQ00000000\n' +
+  'DAU072 IN\n' + 'DAW160\n' + 'DAYBLK\n' + 'DBA01012029\n' +
+  'DBB01011950\n' + 'DBC1\n' + 'DBD01012024\n' + 'DCANONE\n' +
+  'DCBNONE\n' + 'DCDNONE\n' + 'DCFTESTDOCDISCRIM\n' +
+  'DCGUSA\n' + 'DCK1234567890\n' +
+  'DCSDOE\n' + 'DDAN\n' + 'DDB01012000\n' + 'DDEN\n' + 'DDFN\n' + 'DDGN\n';
 const testBytesComplete = encoder.encode(orderedFieldsComplete);
 
 // what to test against here for correctness?
 describe('Canonicalizer Test', function() {
   it('function calls should return correct types', async function() {
-    const parsedData = await parse({data, fields});
+    const parsedData = await parse({data, selector});
     const testHash = await hash({parsedData});
     const testCanonicalized = canonicalize({parsedData});
 
@@ -54,28 +63,38 @@ describe('Canonicalizer Test', function() {
     testCanonicalized.should.be.an('uint8array');
   });
   it('should canonicalize correctly', async function() {
-    const parsedData = await parse({data, fields});
+    const parsedData = await parse({data, selector});
     const canonizedBytes = canonicalize({parsedData});
 
     testBytes.should.deep.equal(canonizedBytes);
   });
   it('should canonicalize + hash correctly', async function() {
     // test against different hasher here?
-    const testHash = await crypto.subtle.digest('SHA-256', testBytes);
+    const testHash = await _sha256(testBytes);
     const testArray = new Uint8Array(testHash);
-    const parsedData = await parse({data, fields});
+    const parsedData = await parse({data, selector});
     const canonizedHash = await hash({parsedData});
 
     testArray.should.deep.equal(canonizedHash);
   });
-  it('should canonicalize correctly without fields param', async function() {
-    const testHash = await crypto.subtle.digest('SHA-256', testBytesComplete);
-    const testArray = new Uint8Array(testHash);
-    const parsedData = await parse({data});
-    const canonizedHash = await hash({parsedData});
+  it('should canonicalize correctly with mandatory selector param',
+    async function() {
+      const testHash = await _sha256(testBytesMandatory);
+      const testArray = new Uint8Array(testHash);
+      const parsedData = await parse({data, selector: {fields: 'mandatory'}});
+      const canonizedHash = await hash({parsedData});
 
-    testArray.should.deep.equal(canonizedHash);
-  });
+      testArray.should.deep.equal(canonizedHash);
+    });
+  it('should canonicalize everything with no selector param',
+    async function() {
+      const testHash = await _sha256(testBytesComplete);
+      const testArray = new Uint8Array(testHash);
+      const parsedData = await parse({data});
+      const canonizedHash = await hash({parsedData});
+
+      testArray.should.deep.equal(canonizedHash);
+    });
   it('should decode raw bytes', async function() {
     const result = await decode({data});
 
@@ -87,7 +106,7 @@ describe('Canonicalizer Test', function() {
       DDB: '01012000'
     };
 
-    const fields = orderedFieldsComplete.split('\n');
+    const fields = orderedFieldsMandatory.split('\n');
     for(const field of fields) {
       const key = field.slice(0, 3);
       const value = field.slice(3);
@@ -145,7 +164,7 @@ describe('Canonicalizer Test', function() {
       DDB: '01012000'
     };
 
-    const fields = orderedFieldsComplete.split('\n');
+    const fields = orderedFieldsMandatory.split('\n');
     for(const field of fields) {
       const key = field.slice(0, 3);
       const value = field.slice(3);
@@ -194,11 +213,15 @@ describe('Canonicalizer Test', function() {
     const raw = pdf417String.replace(/DL/g, 'ID');
     const data = encoder.encode(raw);
 
-    const testHash = await crypto.subtle.digest('SHA-256', testBytes);
+    const testHash = await _sha256(testBytes);
     const testArray = new Uint8Array(testHash);
-    const parsedData = await parse({data, fields});
+    const parsedData = await parse({data, selector});
     const canonizedHash = await hash({parsedData});
 
     testArray.should.deep.equal(canonizedHash);
   });
 });
+
+async function _sha256(data) {
+  return crypto.subtle.digest('SHA-256', data);
+}
